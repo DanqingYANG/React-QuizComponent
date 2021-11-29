078e537749d99fc126d3b2217eed91b51ccce84d
Lena Y <lenayoungydq@gmail.com>
2021-11-29 22:26:31 +0100

    Completed module

---
+++ src/Quiz.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+import QuizQuestion from './QuizQuestion'
+
+let quizData = require('./quiz_data.json')
+
+// class Quiz extends Component
+// {
+//     constructor(props){
+//         super(props)
+//         this.state = {quiz_position: 1}
+//     }
+
+//     render(){
+//         return(
+//             <div>
+//                 <QuizQuestion/>
+//                     {/* quizData.quiz_questions[0].instruction_text */}
+//             </div>
+//         )
+//     }
+// }
+
+function Quiz(props){
+    return(
+        <div>
+            <QuizQuestion/>
+            {quizData.quiz_questions[0].instruction_text}
+        </div>
+    )
+}
+
+export default Quiz
---
+++ src/QuizQuestion.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react' // imrc
+
+// class QuizQuestion extends Component
+// {
+//     render(){
+//         return(
+//             <main>
+//                 <section>
+//                     <p>
+//                         // instruction
+//                         {/* instruction_text, add a prop named quiz_question that passes the value of the quizData.quiz_questions array at the state's quiz_position minus 1. */}
+//                     </p>
+//                 </section>
+//                 {/* <section className="button"> */}
+//                 <section>
+//                     <ul>
+//                         // quiz question
+//                         <li></li>
+//                         {/* this.props.quiz_question.answer_options[0]. */}
+//                     </ul>
+//                 </section>
+//             </main>
+//         )
+//     }
+// }
+
+function QuizQuestion(props){
+    
+    return(
+        <main>
+            <section>
+                <p>
+                    // instruction
+                    {/* instruction_text, add a prop named quiz_question that passes the value of the quizData.quiz_questions array at the state's quiz_position minus 1. */}
+                </p>
+            </section>
+            {/* <section className="button"> */}
+            <section>
+                <ul>
+                    // quiz question
+                    <li></li>
+                    {/* this.props.quiz_question.answer_options[0]. */}
+                </ul>
+            </section>
+        </main>
+    )
+}
+
+export default QuizQuestion
