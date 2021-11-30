d9e2ef0e5a43ddc59f53e31eec652ac50bbdf7b1
Lena Y <lenayoungydq@gmail.com>
2021-11-30 21:33:48 +0100

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,3 +1,4 @@
+import QuizEnd from './QuizEnd'
 import React, {Component} from 'react'
 import QuizQuestion from './QuizQuestion'
 
@@ -12,9 +13,11 @@ class Quiz extends Component
     // console.log(this.state.quiz_position);
 
     render(){
+        const isQuizEnd = (this.state.quiz_position - 1 === quizData.quiz_questions.length)
         return(
             <div>
                 {/* instruction_text, add a prop named quiz_question that passes the value of the quizData.quiz_questions array at the state's quiz_position minus 1. */}
+                {isQuizEnd? <QuizEnd/> : ""}
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
             </div>
         )
